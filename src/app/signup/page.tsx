273b3d668bec35c50cc2fb1457cba0bded432991
damianrobert <damianrobert666@yahoo.com>
2024-04-04 15:24:07 +0300

    .

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,20 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Signup() {
   return (
     <div className="min-h-screen bg-slate-800 text-zinc-300 flex flex-col justify-center items-center">
       <div className="rounded-md p-8 min-w-64 flex flex-col justify-center items-center bg-slate-700">
-        <p className="mb-10 text-3xl tracking-widest font-bold">SIGN IN</p>
+        <p className="mb-10 text-3xl tracking-widest font-bold">SIGN UP</p>
+        <input
+          className="mb-6 rounded-sm px-1 text-gray-800 font-semibold outline-none"
+          type="text"
+          placeholder="First Name"
+        />
+        <input
+          className="mb-6 rounded-sm px-1 text-gray-800 font-semibold outline-none"
+          type="text"
+          placeholder="Last Name"
+        />
         <input
           className="mb-6 rounded-sm px-1 text-gray-800 font-semibold outline-none"
           type="email"
@@ -16,11 +26,11 @@ export default function Home() {
           placeholder="Password"
         />
         <button className="bg-zinc-100 w-full rounded-md p-1 text-slate-800 font-bold text-lg mb-6 tracking-widest">
-          LOGIN
+          SUBMIT
         </button>
 
-        <Link href="signup" className="text-white hover:font-bold">
-          👉 Click here to sign up
+        <Link href="/" className="text-white hover:font-bold">
+          👉 Go to sign in
         </Link>
       </div>
     </div>
