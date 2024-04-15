46054e9f3349428b04851675f7c846c670aba945
Radulescu Roberto-Damian <damianrobert666@yahoo.com>
2024-04-15 21:23:45 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,42 @@
-"use client";
+'use client';
 
-import Button from "@/components/button";
-import Link from "next/link";
-import { UserType } from "@/types/userType";
-import { useState, useEffect } from "react";
-import { Input } from "@/components/ui/input";
+import Button from '@/components/button';
+import Link from 'next/link';
+import { UserType } from '@/types/userType';
+import { useState, useEffect } from 'react';
+import { Input } from '@/components/ui/input';
 
 export default function Home() {
   const [user, setUser] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   });
 
   useEffect(() => {
-    setUser({ email: "", password: "" });
+    setUser({ email: '', password: '' });
   }, []);
 
   return (
-    <div className="min-h-screen bg-slate-800 text-zinc-300 flex flex-col justify-center items-center">
-      <div className="rounded-md p-8 min-w-64 flex flex-col justify-center items-center bg-slate-700 border-2 border-black">
-        <p className="mb-10 text-3xl tracking-widest font-bold">SIGN IN</p>
-        <div className="my-2">
+    <div className='min-h-screen bg-slate-800 text-zinc-300 flex flex-col justify-center items-center'>
+      <div className='rounded-md p-8 min-w-64 flex flex-col justify-center items-center bg-slate-700 border-2 border-black'>
+        <p className='mb-10 text-3xl tracking-widest font-bold'>SIGN IN</p>
+        <div className='my-2'>
           <p>Email</p>
-          <Input className="my-2 text-gray-800" type="email" />
+          <Input
+            className='my-2 text-gray-600'
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
 
           <p>Password</p>
-          <Input className="my-2 text-gray-800" type="email" />
+          <Input
+            className='my-2 text-gray-600'
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
         </div>
-        <Button name="LOG IN"></Button>
-        <Link href="signup" className="text-white hover:font-bold">
+        <Button name='LOG IN'></Button>
+        <Link href='signup' className='text-white hover:font-bold'>
           👉 Click here to sign up
         </Link>
       </div>
