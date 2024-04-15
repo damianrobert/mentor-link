4d87c729ef00c34b7f07f0febee86e4c03293d3e
damianrobert <damianrobert666@yahoo.com>
2024-04-15 16:37:39 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,33 @@
-'use client';
+"use client";
 
 import Button from "@/components/button";
 import Link from "next/link";
 import { UserType } from "@/types/userType";
 import { useState, useEffect } from "react";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  
   const [user, setUser] = useState({
     email: "",
-    password: ""
+    password: "",
   });
 
   useEffect(() => {
     setUser({ email: "", password: "" });
   }, []);
-  
-  
+
   return (
     <div className="min-h-screen bg-slate-800 text-zinc-300 flex flex-col justify-center items-center">
-      <div className="rounded-md p-8 min-w-64 flex flex-col justify-center items-center bg-slate-700">
+      <div className="rounded-md p-8 min-w-64 flex flex-col justify-center items-center bg-slate-700 border-2 border-black">
         <p className="mb-10 text-3xl tracking-widest font-bold">SIGN IN</p>
-        <input
-          className="mb-6 rounded-sm px-1 text-gray-800 font-semibold outline-none"
-          type="email"
-          placeholder="Email"
-          value={user.email || ''}
-          onChange={e => setUser(prev => ({ ...prev, email: e.target.value }))}
-        />
-        <input
-          className="mb-6 rounded-sm px-1 text-gray-800 font-semibold outline-none"
-          type="password"
-          placeholder="Password"
-          value={user.password || ''}
-          onChange={e => setUser(prev => ({ ...prev, password: e.target.value }))}
-        />
-        <Button name="LOG IN"></Button>
+        <div className="my-2">
+          <p>Email</p>
+          <Input className="my-2 text-gray-800" type="email" />
 
+          <p>Password</p>
+          <Input className="my-2 text-gray-800" type="email" />
+        </div>
+        <Button name="LOG IN"></Button>
         <Link href="signup" className="text-white hover:font-bold">
           👉 Click here to sign up
         </Link>
