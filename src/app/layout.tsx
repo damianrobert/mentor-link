cbc838835c7f3250c032c2aa870450c39c49cea9
damianrobert <damianrobert666@yahoo.com>
2024-04-19 12:54:25 +0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={font.className}>
           <main>{children}</main>
         </body>
       </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,15 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-slate-800 text-zinc-300 flex flex-col justify-center items-center">
       <UserButton />
+      <h1>LANDING PAGE</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error nesciunt
+        aspernatur provident. Nostrum, pariatur! Inventore eos magnam,
+        exercitationem blanditiis cumque, voluptate molestiae architecto
+        distinctio optio quia quae porro et quod sit sed tenetur modi ratione
+        aliquid. Voluptatem, nobis. Sapiente laudantium saepe error eveniet
+        facilis sint illum repellat optio odio sunt.
+      </p>
     </div>
   );
 }
