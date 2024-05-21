b2e3b2eaadde3e64e3ea984882b69674e486e85d
Radulescu Roberto-Damian <damianrobert666@yahoo.com>
2024-05-22 00:19:50 +0300

    dev init

---
+++ src/app/(routes)/signup/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Link from 'next/link';
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+function Signup() {
+  return <div>signup page</div>;
+}
+
+export default Signup;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,15 @@
-"use client";
+'use client';
 
-import Button from "@/components/button";
-import Link from "next/link";
-import { UserType } from "@/types/userType";
-import { useState, useEffect } from "react";
-import { Input } from "@/components/ui/input";
-import { UserButton } from "@clerk/nextjs";
+import Button from '@/components/button';
+import Link from 'next/link';
+import { UserType } from '@/types/userType';
+import { useState, useEffect } from 'react';
+import { Input } from '@/components/ui/input';
 
 export default function Home() {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="min-h-screen bg-zinc-200 text-slate-600 flex flex-col justify-center items-center relative">
-      <div className="">
-        <UserButton />
-      </div>
-
-      <div className="absolute top-[2rem] ">
-        <div className="flex text-[6rem] font-bold">
-          <p className="text-[#4490db]">Mentor</p>
-          <p className="text-[#44cb8c]">Link</p>
-        </div>
-      </div>
-    </div>
+    <div className='min-h-screen bg-zinc-200 text-slate-600 flex flex-col justify-center items-center relative'></div>
   );
 }
