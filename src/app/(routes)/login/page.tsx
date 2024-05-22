a96dbffbbadbba2cfee364d63ea280f478e41ac5
damianrobert <damianrobert666@yahoo.com>
2024-05-22 09:43:04 +0300

    env

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -1,11 +1,5 @@
 function Login() {
-  return (
-    <div>
-      login page
-      'DATABASE_URL="mongodb+srv://damianrobert666:xP042dGg8aEf7BuH@mentorlink.p0epaln.mongodb.net/MentorLinkDB?retryWrites=true&w=majority&appName=mentorLink"
-      TOKEN_SECRET="d&97&*(GHu8_3f#9HaF8h@f0D" DOMAIN=http://localhost:3000'
-    </div>
-  );
+  return <div>login page</div>;
 }
 
 export default Login;
