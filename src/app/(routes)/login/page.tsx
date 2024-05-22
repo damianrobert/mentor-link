283a148329401bcd3c7c6c35f30d2de934cd8d6e
damianrobert <damianrobert666@yahoo.com>
2024-05-22 09:42:17 +0300

    rmv env

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -1,5 +1,11 @@
 function Login() {
-  return <div>login page</div>;
+  return (
+    <div>
+      login page
+      'DATABASE_URL="mongodb+srv://damianrobert666:xP042dGg8aEf7BuH@mentorlink.p0epaln.mongodb.net/MentorLinkDB?retryWrites=true&w=majority&appName=mentorLink"
+      TOKEN_SECRET="d&97&*(GHu8_3f#9HaF8h@f0D" DOMAIN=http://localhost:3000'
+    </div>
+  );
 }
 
 export default Login;
